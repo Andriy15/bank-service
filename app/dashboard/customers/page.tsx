2fe3570e06f7a038ec51182837ff4a8501eb26d3
Andrii <andriychikulay@gmail.com>
2024-02-15 01:10:25 +0200

    add logic

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,9 @@
-export default function Page() {
-  return (
-     <div>
-       <h1>Customers Page</h1>
-     </div>
-  )
+import Table from '@/app/ui/customers/table'
+export default async function Page() {
+	return (
+		<div className="w-full">
+			<h1 className="mb-8 text-xl md:text-2xl">Customers</h1>
+			{/*<Table customers={} />*/}
+		</div>
+	)
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,12 @@
-import SideNav from "@/app/ui/dashboard/sidenav";
+import SideNav from '@/app/ui/dashboard/sidenav'
 
-export default function Layout({children}: { children: React.ReactNode }) {
-  return (
-     <div className='flex flex-col h-screen md:flex-row md:overflow-hidden'>
-       <div className="w-full flex-none md:w-64">
-         <SideNav/>
-       </div>
-       <div className='flex-1 p-4'>{children}</div>
-     </div>
-  )
+export default function Layout({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="flex flex-col md:flex-row md:overflow-hidden">
+			<div className="w-full flex-none md:w-64">
+				<SideNav />
+			</div>
+			<div className="flex-1 p-4">{children}</div>
+		</div>
+	)
 }
